fc31786c33ba97f58b2e659539fdfa74893fb1b8
调调 <1096512323@qq.com>
2023-07-28 09:05:44 +0800

    添加项目文件。

---
+++ PluginDemo/MyConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace ClassLibrary1
+{
+    internal sealed class MyConfig
+    {
+        [Description("是否启用")]
+        public bool Enable { get; set; } = true;
+    }
+}
---
+++ PluginDemo/MyPlugin.cs
@@ -0,0 +1,20 @@
+﻿using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+
+namespace ClassLibrary1
+{
+    internal class MyPlugin
+    {
+        public static MyPlugin myPlugin { get; private set; }
+
+        [PluginEntryPoint("我的插件", "1.0.0", "我的第一个插件.", "作者")]
+        void LoadPlugin()
+        {
+            myPlugin = this;
+            EventManager.RegisterEvents(this);
+        }
+
+        [PluginConfig]
+        public MyConfig DDconfig;
+    }
+}
